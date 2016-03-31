555960a5fe0f0333879829baf34c61446b57caa2
Thomas Binu <thomas.binu@gistec.com>
2016-04-01 01:52:03 +0400

    Added a lot more questions

--- Level2.py
+++ Level2.py
@@ -1,4 +1,4 @@
-import math
+import math, re
 from ast import literal_eval
 
 
@@ -66,11 +66,11 @@ def sort_words():
 def capitalize_lines():
     lines = []
     s = " "
-    print("Enter a bunch of lines:\n")
+    print("Enter a bunch of lines")
     while s:
         s = input()
         if s:
-            lines.append(s)
+            lines.append(s.upper())
     print("\n".join(lines))
 
 
@@ -79,8 +79,8 @@ def capitalize_lines():
 #
 # Question:
 # Write a program which accepts a sequence of comma separated 4 digit binary numbers as
-# its input and then check whether they are divisible by 5 or not. The numbers that are
-# divisible by 5 are to be printed in a comma separated sequence.
+# its input and then check whether they are divisible by 5 or not.
+# The numbers that are divisible by 5 are to be printed in a comma separated sequence.
 # Example:
 # 0100,0011,1010,1001
 # Then the output should be:
@@ -91,5 +91,147 @@ def check_divisibility():
     div_numbers = [x for x in binary if int(x, 2) % 5 == 0]
     print(",".join(div_numbers))
 
+
+# Question 10
+# Level 2
+#
+# Question:
+# Write a program that accepts a sequence of whitespace separated words as input
+# and prints the words after removing all duplicate words and sorting them alphanumerically.
+# Suppose the following input is supplied to the program:
+# hello world and practice makes perfect and hello world again
+# Then, the output should be:
+# again and hello makes perfect practice world
+def remove_duplicate():
+    print(" ".join(sorted(set(input("Enter a sentence:").split(" ")))))
+
+
+# Question 12
+# Level 2
+#
+# Question:
+# Write a program, which will find all such numbers between 1000 and 3000 (both included) such
+# that each digit of the number is an even number.
+# The numbers obtained should be printed in a comma-separated sequence on a single line.
+#
+# Hints:
+# In case of input data being supplied to the question, it should be assumed to be a console input.
+def even_digits():
+    even_digit = [str(number) for number in range(1000, 3001) if all([int(digit) % 2 == 0 for digit in str(number)])]
+    print(",".join(even_digit))
+
+
+# Question 13
+# Level 2
+#
+# Question:
+# Write a program that accepts a sentence and calculate the number of letters and digits.
+# Suppose the following input is supplied to the program:
+# hello world! 123
+# Then, the output should be:
+# LETTERS 10
+# DIGITS 3
+def letter_digit_count():
+    sentence = input("Enter a sentence of digits and numbers:");
+    count = {'letter':0 , 'number':0}
+    for letter in sentence:
+        if letter.isalpha():
+            count['letter'] += 1
+        elif letter.isdigit():
+            count['number'] += 1
+    print("LETTERS:" + str(count['letter']) + "\nNUMBERS:" + str(count['number']))
+
+
+# Question 14
+# Level 2
+#
+# Question:
+# Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+# Suppose the following input is supplied to the program:
+# Hello world!
+# Then, the output should be:
+# UPPER CASE 1
+# LOWER CASE 9
+def case_count():
+    sentence = input("Enter a sentence of digits and numbers:");
+    count = {'upper': 0, 'lower': 0}
+    for letter in sentence:
+        if letter.isupper():
+            count['upper'] += 1
+        elif letter.islower():
+            count['lower'] += 1
+    print("UPPER:" + str(count['upper']) + "\nLOWER:" + str(count['lower']))
+
+
+# Question 15
+# Level 2
+#
+# Question:
+# Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+# Suppose the following input is supplied to the program:
+# 9
+# Then, the output should be:
+# 11106
+def cumulative_addition():
+    n, a = literal_eval(input("Enter n,a ?: "))
+    ans = 0
+    for i in range(1, n+1):
+        ans += (int((((10 ** i) - 1)/9)) * a)
+    print(ans)
+
+
+# Question 16
+# Level 2
+#
+# Question:
+# Use a list comprehension to square each odd number in a list.
+# The list is input by a sequence of comma-separated numbers.
+# Suppose the following input is supplied to the program:
+# 1,2,3,4,5,6,7,8,9
+# Then, the output should be:
+# 1,9,25,49,81
+
+def square_numbers():
+    numbers = literal_eval(input("Enter comma separated list of numbers:"))
+    print(",".join([str(n*n) for n in numbers if n % 2 == 1]))
+
+
+# Question:
+# Write a program that computes the net amount of a bank account based a transaction log from console input.
+# The transaction log format is shown as following:
+# D 100
+# W 200
+#
+# D means deposit while W means withdrawal.
+# Suppose the following input is supplied to the program:
+# D 300
+# D 300
+# W 200
+# D 100
+# Then, the output should be:
+# 500
+def bank():
+
+    balance = 0
+    print("Enter transaction in format - transaction(D/W) amount(100):")
+    while True:
+
+        transaction = input()
+        if not transaction:
+            break
+        else:
+
+            if re.match("^[DdWw]\s\d+$", transaction):
+                flow, amount = transaction.split(" ")
+                if flow.upper() == 'D':
+                    balance += int(amount)
+                elif flow.upper() == 'W':
+                    balance -= int(amount)
+            else:
+                print("Please enter the correct format")
+                continue
+    print("The final amount is {}".format(balance))
+
+
 if __name__ == "__main__":
-    check_divisibility()
+    bank()
